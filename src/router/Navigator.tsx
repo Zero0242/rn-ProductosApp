1207cba564fdb7ea85416fc1459fecd2e0a82e94
Zero0242 <pedrocorream66@gmail.com>
2023-12-01 02:13:13 -0300

    rutas autenticado

--- src/router/Navigator.tsx
+++ src/router/Navigator.tsx
@@ -1,19 +1,29 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useContext } from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { LoginScreen, ProtectedScreen, RegisterScreen } from '../screens';
+import { AuthContext } from '../context/authContext';
 
 const Stack = createNativeStackNavigator();
 
 export const Navigator = () => {
+    const { status } = useContext(AuthContext)
     return (
         <Stack.Navigator screenOptions={{
             header: () => null,
             contentStyle: { backgroundColor: 'white' }
         }}>
-            <Stack.Screen name='LoginScreen' component={LoginScreen} />
-            <Stack.Screen name='RegisterScreen' component={RegisterScreen} />
-            <Stack.Screen name='ProtectedScreen' component={ProtectedScreen} />
+            {
+                (status !== 'logged-in')
+                    ?
+                    (
+                        <>
+                            <Stack.Screen name='LoginScreen' component={LoginScreen} />
+                            <Stack.Screen name='RegisterScreen' component={RegisterScreen} />
+                        </>
+                    )
+                    : <Stack.Screen name='ProtectedScreen' component={ProtectedScreen} />
+            }
         </Stack.Navigator>
     )
 }
