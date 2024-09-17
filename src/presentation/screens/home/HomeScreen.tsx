fd7632bf66aa55d5577ac1a7609096e2ef1af93f
Zero0242 <pedrocorream66@gmail.com>
2024-09-17 01:16:49 -0300

    [Logout]: cerrar sesión

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,15 +1,22 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
+import { Button, Layout, Text } from '@ui-kitten/components'
 import React from 'react'
-import { Text, View } from 'react-native'
 import { RootStackParams } from '../../router'
+import { useAuthStore } from '../../store'
 
 interface Props extends NativeStackScreenProps<RootStackParams, 'HomeScreen'> { }
 
 export function HomeScreen({ navigation, route }: Props) {
+    const logout = useAuthStore(state => state.logout)
+
     return (
-        <View>
+        <Layout>
             <Text>HomeScreen</Text>
-        </View>
+
+            <Button onPress={logout}>
+                Salir
+            </Button>
+        </Layout>
     )
 }
 
