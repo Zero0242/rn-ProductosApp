345de7f37543cabc30b454dacbc4b708159ca9ad
Zero0242 <pedrocorream66@gmail.com>
2024-09-18 18:23:28 -0300

    [ReactQuery]: configuracion provider
    > Ajustes del MainLayout

--- src/presentation/layouts/MainLayout.tsx
+++ src/presentation/layouts/MainLayout.tsx
@@ -1,4 +1,5 @@
 import { Divider, Layout, TopNavigation, TopNavigationAction } from "@ui-kitten/components";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { AppIcon } from "../components/ui";
 import { useAppRouter } from "../hooks";
 
@@ -7,15 +8,28 @@ interface Props {
     title: string
     subtitle: string
     children: React.ReactNode
+    actions?: {
+        icon: string
+        onPress: () => void
+    }
 }
 
-export function MainLayout({ children, subtitle, title }: Props) {
+export function MainLayout({ children, subtitle, title, actions }: Props) {
     const { canGoBack, goBack } = useAppRouter()
+    const { top } = useSafeAreaInsets()
 
     const backButton = () => {
         return <TopNavigationAction
             onPress={goBack}
             icon={() => <AppIcon name="arrow-back-outline" />}
+        />
+    }
+
+    const RightButton = () => {
+        if (!actions) return undefined
+        return <TopNavigationAction
+            icon={() => <AppIcon name={actions.icon} />}
+            onPress={actions.onPress}
         />
     }
 
@@ -25,8 +39,9 @@ export function MainLayout({ children, subtitle, title }: Props) {
                 title={title}
                 subtitle={subtitle}
                 accessoryLeft={canGoBack() ? backButton : undefined}
+                accessoryRight={() => <RightButton />}
                 alignment="center"
-                style={{ backgroundColor: '#f9f9f9' }}
+                style={{ backgroundColor: '#f9f9f9', paddingTop: top }}
             />
             <Divider />
             <Layout style={{ height: '100%' }}>
--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,28 +1,35 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
-import { Button, Text } from '@ui-kitten/components'
+import { useQuery } from '@tanstack/react-query'
+import { List, Text } from '@ui-kitten/components'
+import { getProductsByPage } from '../../../actions/products'
+import { FullLoad } from '../../components/ui'
 import { MainLayout } from '../../layouts'
 import { RootStackParams } from '../../router'
-import { useAuthStore } from '../../store'
 
 interface Props extends NativeStackScreenProps<RootStackParams, 'HomeScreen'> { }
 
 export function HomeScreen({ navigation, route }: Props) {
-    const logout = useAuthStore(state => state.logout)
-    const user = useAuthStore(state => state.user)
+    const { isLoading, data: products = [] } = useQuery({
+        queryKey: ['products', 'infinite'],
+        staleTime: 1000 * 60 * 60,
+        queryFn: () => getProductsByPage(0)
+    })
 
     return (
         <MainLayout
             title="Home"
             subtitle="Bienvenido a la aplicación"
         >
-            <Text>HomeScreen {user?.fullName ?? ''}</Text>
+            {
+                isLoading ? <FullLoad />
+                    : <List
+                        data={products}
+                        numColumns={2}
+                        keyExtractor={(item, index) => `${item.id}-${index}`}
+                        renderItem={({ item }) => <Text> Product {item.id} </Text>}
+                    />
+            }
 
-            <Button onPress={logout}>
-                Salir
-            </Button>
-            <Button onPress={() => navigation.navigate('ProductScreen', { productId: "asdasda" })}>
-                Next
-            </Button>
         </MainLayout>
     )
 }
