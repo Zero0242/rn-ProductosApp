9cd96b4bd47c50afddec4b71643c5753a80084fe
Zero0242 <pedrocorream66@gmail.com>
2025-02-28 15:56:38 -0300

    Update Field.tsx

--- src/presentation/shared/components/Field.tsx
+++ src/presentation/shared/components/Field.tsx
@@ -22,6 +22,7 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         borderColor: 'black',
         borderWidth: 1,
+        backgroundColor: 'white',
         width: '100%',
         marginVertical: 10,
         fontFamily: 'SpaceMono',
