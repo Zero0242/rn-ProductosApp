5c93cee19ec5928c8e3b0a6964c4cffc90469ba8
Zero0242 <pedrocorream66@gmail.com>
2025-02-28 16:10:06 -0300

    Update AuthView.tsx
    > ajustes de layout en auth

--- src/presentation/auth/components/AuthView.tsx
+++ src/presentation/auth/components/AuthView.tsx
@@ -1,13 +1,18 @@
-import React, { PropsWithChildren } from 'react'
-import { Keyboard, StyleSheet, TouchableWithoutFeedback, View } from 'react-native'
+import React, { PropsWithChildren } from 'react';
+import { Image, Keyboard, KeyboardAvoidingView, Platform, StatusBar, StyleSheet, TouchableWithoutFeedback } from 'react-native';
 
 export function AuthView({ children }: PropsWithChildren) {
     return (
         <>
+            <StatusBar animated hidden />
             <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-                <View style={styles.container}>
+                <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+                    <Image
+                        style={styles.image}
+                        source={require('@/assets/images/react-logo.png')}
+                    />
                     {children}
-                </View>
+                </KeyboardAvoidingView>
             </TouchableWithoutFeedback>
         </>
     )
@@ -16,9 +21,15 @@ export function AuthView({ children }: PropsWithChildren) {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        color: 'transparent',
         alignItems: 'flex-start',
         justifyContent: 'center',
         paddingHorizontal: 20
     },
+    image: {
+        width: 200,
+        height: 200,
+        position: 'absolute',
+        top: -20,
+        right: -20
+    }
 })
